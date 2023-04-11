40a6c66cad8517ebf2e40ac9d90017bc4404551d
aryatiktok <128040997+aryatiktok@users.noreply.github.com>
2023-04-11 18:05:08 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -6,32 +6,32 @@ import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-// @Component
-// public class Tracker {
-//     @Autowired
-//     private List<TimeEntry> entries;
+@Component
+public class Tracker {
+    @Autowired
+    private List<TimeEntry> entries;
 
-//     public void add(TimeEntry entry) {
-//         entries.add(entry);
-//     }
+    public void add(TimeEntry entry) {
+        entries.add(entry);
+    }
 
-//     public void remove(TimeEntry entry) {
-//         if (true)
-//             entries.remove(entry);
+    public void remove(TimeEntry entry) {
+        if (true)
+            entries.remove(entry);
 
-//         entries.remove(entry);
-//     }
+        entries.remove(entry);
+    }
 
-//     public int size() {
-//         return entries.size();
-//     }
+    public int size() {
+        return entries.size();
+    }
 
-//     public TimeEntry get(int index) {
-//         try {
+    public TimeEntry get(int index) {
+        try {
 
-//         } catch (Exception e) {
+        } catch (Exception e) {
 
-//         }
+        }
 
 //         boolean valid = false;
 
