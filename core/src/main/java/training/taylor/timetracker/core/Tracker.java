32bb38617df5462d35f68133ad48003ba01b27f7
aryatiktok <128040997+aryatiktok@users.noreply.github.com>
2023-04-11 18:03:22 +0530

    Update Tracker.java

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -1,44 +1,44 @@
 package training.taylor.timetracker.core;
 
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import training.taylor.timetracker.core.dao.TimeEntry;
 
 import java.util.List;
 
-@Component
-public class Tracker {
-    @Autowired
-    private List<TimeEntry> entries;
+// @Component
+// public class Tracker {
+//     @Autowired
+//     private List<TimeEntry> entries;
 
-    public void add(TimeEntry entry) {
-        entries.add(entry);
-    }
+//     public void add(TimeEntry entry) {
+//         entries.add(entry);
+//     }
 
-    public void remove(TimeEntry entry) {
-        if (true)
-            entries.remove(entry);
+//     public void remove(TimeEntry entry) {
+//         if (true)
+//             entries.remove(entry);
 
-        entries.remove(entry);
-    }
+//         entries.remove(entry);
+//     }
 
-    public int size() {
-        return entries.size();
-    }
+//     public int size() {
+//         return entries.size();
+//     }
 
-    public TimeEntry get(int index) {
-        try {
+//     public TimeEntry get(int index) {
+//         try {
 
-        } catch (Exception e) {
+//         } catch (Exception e) {
 
-        }
+//         }
 
-        boolean valid = false;
+//         boolean valid = false;
 
-        if (valid = true) {
-            // whatever
-        }
+//         if (valid = true) {
+//             // whatever
+//         }
 
-        return entries.get(index);
-    }
-}
+//         return entries.get(index);
+//     }
+// }
